4096c0ce679224b074321f08a15ba81feeb6f882
skentos1 <kentos.simon@gmail.com>
2025-03-17 09:37:02 +0100

    zmena cookies v logine

--- server/routes/pouzivatel.js
+++ server/routes/pouzivatel.js
@@ -126,7 +126,7 @@ router.post("/login", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       path: "/", // Ensure the path matches the clearCookie method
       maxAge: 5 * 60 * 60 * 1000, // 5 hours
     });
